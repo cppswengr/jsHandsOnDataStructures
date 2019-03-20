e901a40383ce0b93c02f3c887ff8ee85dc373009
cppswengr <cppswengr@eVantageGroup.com>
2019-03-19 22:12:54 -0500

    Working on back-button, top p23

--- back-button/src/app/app.component.ts
+++ back-button/src/app/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Stack } from './utils/stack';
 
-constructor(private stack: Stack, private router: Router) {
-
-  // sunbscrine to the routers event
-  this.router.events.subscribe((val) => {
-
-    // determine if router is telling us that is has ended transition
-    if (val instanceof NavigationEnd) {
-
-      // state change done, add to stack
-      this.stack.push(val);
-    }
-  });
-}
 
 @Component({
   selector: 'app-root',
@@ -22,6 +9,19 @@ constructor(private stack: Stack, private router: Router) {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private stack: Stack, private router: Router) {
+
+    // sunbscrine to the routers event
+    this.router.events.subscribe((val) => {
+
+      // determine if router is telling us that is has ended transition
+      if (val instanceof NavigationEnd) {
+
+        // state change done, add to stack
+        this.stack.push(val);
+      }
+    });
+  }
   title = 'back-button';
 }
 
