940f40bea3ce4908a57390bb9fca5a086dfaf09c
cppswengr <cppswengr@eVantageGroup.com>
2019-03-20 17:30:49 -0500

    top page 26

--- back-button/src/app/app.component.ts
+++ back-button/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Stack } from './utils/stack';
 
@@ -6,22 +6,29 @@ import { Stack } from './utils/stack';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './theme.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class AppComponent {
   constructor(private stack: Stack, private router: Router) {
-
-    // sunbscrine to the routers event
     this.router.events.subscribe((val) => {
-
-      // determine if router is telling us that is has ended transition
       if (val instanceof NavigationEnd) {
-
-        // state change done, add to stack
         this.stack.push(val);
       }
     });
   }
-  title = 'back-button';
+
+  goBack() {
+    let current = this.stack.pop();
+    let prev = this.stack.peek();
+
+    if (prev) {
+      this.stack.pop();
+      this.router.navigateByUrl(prev.urlAfterRedirects);
+    } else {
+      this.stack.push(current);
+    }
+  }
 }
 
