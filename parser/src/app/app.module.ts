1d7f8c92426d5cd1161592fe747c67c51750f61a
cppswengr <cppswengr@eVantageGroup.com>
2019-03-21 15:50:59 -0500

    top of page 28; parser build errors

--- parser/src/app/app.module.ts
+++ parser/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule, MatGridListModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+
+        // material
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatGridListModule
+    ],
+    providers: [
+
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
